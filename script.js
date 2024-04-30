ef7b3cd3240e28a56a1c070e19fe0617db96a684
KingsMunene <munenekings2@gmail.com>
2024-04-30 10:49:18 +0300

    "Updated script.js: changed ul selector to '.toDoList', added event listener for ul element to remove items, refactored addToDoItem and added addItemToArray"

--- script.js
+++ script.js
@@ -5,24 +5,54 @@
 
         let form = document.querySelector('.form');
         let input = form.querySelector('.form_input');
-        let ul = document.querySelector('.list');
+        let ul = document.querySelector('.toDoList');
         
         form.addEventListener('submit', (e) =>{
             e.preventDefault();
 
             let toDoItem = input.value;
             let itemID = String(Date.now());
-            
+            addToDoItem(itemID, toDoItem);
+            addItemToArray(itemID,toDoItem);
+            input.value ="";
         });
 
+        ul.addEventListener('click', (e) => {
+            let id = e.target.getAttribute('toDoId');
+            console.log(id)
+            if(!id) return;
+            removeItemFromDOM(id);
+            removeItemFromArray(id);
+        });
+
+        function removeItemFromDOM(id){
+            ul.removeChild(document.querySelector(`[toDoId="${id}"]`));
+        }
+
+        function removeItemFromArray(id){
+            let index = toDoList.findIndex(item => item.itemID == id);
+            if(index != -1){
+                toDoList.splice(index, 1);
+                console.log('Item deleted!!')
+            }
+        }
+
+
+
+        function addToDoItem(itemID, todoItem ){
+            let li = document.createElement('li');
+            li.setAttribute('toDoId', itemID);
+        
+            li.textContent = todoItem;
+            // Add the new list item element to the unordered list
+        
+            ul.appendChild(li);
+        }
+
+        function addItemToArray(itemID, toDoItem){
+    
+            toDoList.push({itemID, toDoItem});
+            console.log(toDoList);
+        }
 })();
 
-
-function addToDoItem(itemID, todoItem ){
-    let li = document.createElement('li');
-    li.setAttribute('id', todoItem)
-
-    li = (itemID, todoItem)
-
-    ul.addC
-}
